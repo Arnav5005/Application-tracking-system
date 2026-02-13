d21bc44e07d23565bdbc0aeca09de65692f6cd49
Arnav pratap singh <arnavpratapsingh2006@gmail.com>
2026-02-13 13:56:24 +0530

    nodemon

--- server.js
+++ server.js
@@ -8,9 +8,10 @@ import {GoogleGenAI} from "@google/genai"
 
 dotenv.config()
 
-const port=process.env.PORT
+const port=process.env.PORT || 1000
 
 const server=express()
 
-server.listen(1000, function(){
+server.listen(port, function(){
+    console.log(`App is running at port : ${port}`)
 })
